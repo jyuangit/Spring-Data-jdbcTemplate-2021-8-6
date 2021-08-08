7029f9be954b1d7f86e9b22c3333c30cfc033596
jyuangit <jiyuan233@qq.com>
2021-08-08 15:29:53 +0800

    first commit

--- src/main/java/com/example/SpringDataJDBC/UserRepository.java
+++ src/main/java/com/example/SpringDataJDBC/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.SpringDataJDBC;
 
 import com.example.SpringDataJDBC.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -13,17 +14,23 @@ public class UserRepository {
 
   private final JdbcTemplate jdbcTemplate;
 
+
   @Autowired
   public UserRepository(JdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
   }
 
   public List<User> findAll() {
-    return null;
+//    Object list = jdbcTemplate.execute("select * from users");
+    String sql="select * from users";
+    return jdbcTemplate.query(sql, new BeanPropertyRowMapper(User.class) );
   }
 
   public Optional<User> findByUsername(String username) {
-    return null;
+//    final Optional<User> empty = (Optional<User>) Optional.ofNullable(jdbcTemplate.queryForObject("select * from users where id =  ?", new Object[]{username}, new BeanPropertyRowMapper(User.class)));
+    return Optional.ofNullable(jdbcTemplate.queryForObject("select * from users where username =  ?"
+            ,new Object[] {username}
+          , (rs, i) -> new User(rs.getLong("id"), rs.getString("name"), rs.getString("username"))));
   }
 
   public Optional<User> findById(Long id) {
---
+++ src/main/java/com/example/SpringDataJDBC/config/BaseConfig.java
@@ -0,0 +1,26 @@
+package com.example.SpringDataJDBC.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class BaseConfig {
+
+    @Bean
+    CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**", configuration);
+//        if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+//            httpServletResponse.setStatus(HttpStatus.OK.value());
+//            return false;
+//        }
+        return new CorsFilter(source);
+    }
+}
